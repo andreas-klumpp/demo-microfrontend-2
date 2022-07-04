ee7526e842c86e028dc37d7889226db7ee266b98
andreas-klumpp <andreas.klumpp@thoughtworks.com>
2022-07-04 09:18:07 +0200

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component<{}, { randomNumber: number }> {
+
+  constructor(props: any) {
+    super(props);
+    this.state =  { randomNumber: 0 };
+
+    this.generateRandomNumber = this.generateRandomNumber.bind(this);
+  }
+
+  generateRandomNumber(): void {
+    this.setState({
+      randomNumber: Math.floor(Math.random() * 100)
+    });
+  }
+
+  render() {
+    return (
+      <div className="MicroFrontend2">
+        <h1>Micro Frontend 2</h1>
+        <div className='RandomNumberContainer'>
+          <p>{this.state.randomNumber}</p>
+          <button onClick={this.generateRandomNumber}>Generate random number</button>
+        </div>
+        <p>Number of Micro Frontend 1: XY</p>
+      </div>
+    );
+  }
 }
 
 export default App;
